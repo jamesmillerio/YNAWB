25f6678c3201a270b64521ac48bab12c6611d632
James Miller <jrmiller84@gmail.com>
2015-02-26 01:10:04 -0500

    Adding code that will pull config values from environment variables. This is for a Heroku deployment.

--- configuration.go
+++ configuration.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"reflect"
+	"strconv"
+	"strings"
 )
 
 type DropboxConfiguration struct {
@@ -57,23 +60,93 @@ type YNABDevice struct {
 
 func LoadConfiguration(path string) Configuration {
 
-	file, e := ioutil.ReadFile(path)
-
-	if e != nil {
-		fmt.Printf("Error reading configuration file: %v\n", e)
-		os.Exit(1)
-	}
-
+	env := GetEnvironmentVariables()
 	config := Configuration{}
 
-	e = json.Unmarshal(file, &config)
+	/* If we're hosting on Heroku, load all
+	 * settings from environment variables. */
+	if env["IS_HEROKU"] == "true" {
 
-	if e != nil {
-		fmt.Printf("Error unmarshalling configuration file: %v\n", e)
-		os.Exit(1)
+		configValue := reflect.ValueOf(&config).Elem()
+		configType := configValue.Type()
+
+		getConfiguration(configType, configValue, "")
+
+		fmt.Printf("Final configuration: %v\n", config)
+
+	} else {
+
+		/* If we're hosting anywhere, just grab
+		 * it from our json configuration file. */
+		file, e := ioutil.ReadFile(path)
+
+		if e != nil {
+			fmt.Printf("Error reading configuration file: %v\n", e)
+			os.Exit(1)
+		}
+
+		e = json.Unmarshal(file, &config)
+
+		if e != nil {
+			fmt.Printf("Error unmarshalling configuration file: %v\n", e)
+			os.Exit(1)
+		}
+
 	}
 
 	return config
+}
+
+func getConfiguration(t reflect.Type, v reflect.Value, parent string) {
+
+	env := GetEnvironmentVariables()
+
+	for i := 0; i < t.NumField(); i++ {
+
+		field := t.Field(i)
+		fieldType := field.Type
+		fieldValue := v.Field(i)
+		key := ""
+
+		if parent == "" {
+			key = strings.ToUpper(field.Name)
+		} else {
+			key = parent + "_" + strings.ToUpper(field.Name)
+		}
+
+		value := env[key]
+
+		fmt.Printf("key: %v, value: %v\n", key, value)
+
+		switch fieldType.Name() {
+		case "string":
+			fieldValue.SetString(value)
+		case "int":
+			if i, err := strconv.Atoi(value); err != nil {
+				fieldValue.SetInt(int64(i))
+			}
+		default:
+			getConfiguration(fieldType, fieldValue, key)
+		}
+
+	}
+
+}
+
+func GetEnvironmentVariables() map[string]string {
+
+	m := make(map[string]string)
+
+	for _, e := range os.Environ() {
+
+		kv := strings.Split(e, "=")
+
+		m[kv[0]] = kv[1]
+
+	}
+
+	return m
+
 }
 
 func LoadYNABSettings(data []byte) YNABSettings {
